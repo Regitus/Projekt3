7ac11ed28040ae63e94da8ab887bd2bb20950926
Regitus <33863615+Regitus@users.noreply.github.com>
2018-12-17 15:08:01 +0100

    Update TestMain.java

--- src/testding/TestMain.java
+++ src/testding/TestMain.java
@@ -1,11 +1,29 @@
 package testding;
 
+
 public class TestMain
 {
 
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
+		/*
+		 * Hamster "Klebstoffklasse"
+		 * muss die Methode vor besitzen
+		 * linksum
+		 * init
+		 * Territorium aus der Antwort lesen.
+		 * 
+		 * Algorithmus aus der Hamsterklasse übernehmen!
+		 * Server/TextmessageSend und getMessage
+		 * 
+		 * Neue Klasse für Territorium
+		 * 
+		 * Leichter KLassenblick
+		 * 
+		 * Territorium<-OriginalerHamsterAlgo -> Hamsterklasse
+		 * 
+		 */
 
 	}
 
