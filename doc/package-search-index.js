cd988c78bcdf4d1aac93de8674835ff3b55d3664
Regitus <33863615+Regitus@users.noreply.github.com>
2019-01-08 14:54:11 +0100

    JavaDoc Anpassen

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"projekt.basis"},{"l":"projekt.hamster"},{"l":"projekt.main"},{"l":"projekt.nachrichten"},{"l":"projekt.suchlogik"},{"l":"projekt.user"},{"l":"testding"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"project.basis"},{"l":"projekt.hamster"},{"l":"projekt.main"},{"l":"projekt.messages"},{"l":"projekt.searchlogic"},{"l":"projekt.user"}]
